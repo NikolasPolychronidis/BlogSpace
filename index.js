0da786cc181b056b276ef872a8be46d8a93ec5e6
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-05-16 11:25:48 +0200

    Add post functionality

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+let postsArray = [];
+function renderPosts() {
+  let html = postsArray
+    .map(post => `<h3>${post.title}</h3><p>${post.body}</p><hr/>`)
+    .join('');
+  document.getElementById('blog-list').innerHTML = html;
+}
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
   .then(response => response.json())
   .then(data => {
-    const postArray = data.slice(0, 5);
-    const html = postArray
-      .map(post => `<h3>${post.title}</h3><p>${post.body}</p><hr/>`)
-      .join('');
-    document.getElementById('blog-list').innerHTML = html;
+    postsArray = data.slice(0, 5);
+    renderPosts();
   });
 
 const form = document.getElementById('form');
@@ -17,5 +22,16 @@ form.addEventListener('submit', function (event) {
     title: postTitle,
     body: postBody,
   };
-  console.log(data);
+  fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => res.json())
+    .then(post => {
+      postsArray.unshift(post);
+      renderPosts();
+    });
 });
